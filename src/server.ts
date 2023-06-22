e20feb788477f6085c9cb6f3e6c150cbf4027977
Deyvid Tavares <devdeyvid2022@gmail.com>
2023-06-21 21:29:04 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import { UsersRoutes } from './routes/user.routes'
 import { SchedulesRoutes } from './routes/schedules.routes'
+import cors from 'cors'
 
 
 const app:Application = express()
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 const usersRoutes = new UsersRoutes().getRoutes()
 const schedulesRoutes = new SchedulesRoutes().getRoutes()
-
+app.use(cors())
 app.use('/users', usersRoutes)
 app.use('/schedules', schedulesRoutes)
 app.use((err: Error, req: Request, res: Response, next: NextFunction ) => {
